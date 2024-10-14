cdbfdd75ee60657a1d5b9a0ba38932615027d9de
Dilshod <dilshodakromboyev@gmail.com>
2024-10-14 14:27:52 +0500

    finish

---
+++ main.js
@@ -0,0 +1,20 @@
+
+    let onatili = +prompt("Matematikadan olgan balingizini kiriting");
+let chettili= +prompt("Ingliz tilidan olgan balingizini kiriting");
+let rustili = +prompt("Rus tilidan olgan balingizini kiriting");
+let kimyo = +prompt("kimyo olgan balingizini kiriting");
+let geografiya = +prompt("geogrefiya olgan balingizini kiriting");
+
+alert(
+  `Sizning umumiy o'rtacha balingiz: ${
+    (onatili + chettili + rustili + kimyo+geografiya) / 5
+  }`
+);
+
+console.log(
+  `Sizning umumiy o'rtacha balingiz: ${
+    (onatili + chettili + rustili + kimyo + geografiya) / 5
+  }`
+);
+
+
